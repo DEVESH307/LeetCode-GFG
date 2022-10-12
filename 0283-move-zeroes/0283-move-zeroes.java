5bccd62ff19944062f0386490586f6d7cb692553
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-13 02:02:06 +0530

    Time: 2 ms (86.44%), Space: 55.4 MB (5.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // int i = 0;
+        // for(int it: nums){
+        //     if(it != 0){
+        //         nums[i++] = it;
+        //     }
+        // }
+        // while(i < nums.length){
+        //     nums[i++] = 0;
+        // }
         int i = 0;
-        for(int it: nums){
-            if(it != 0){
-                nums[i++] = it;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
             }
-        }
-        while(i < nums.length){
-            nums[i++] = 0;
         }
     }
 }

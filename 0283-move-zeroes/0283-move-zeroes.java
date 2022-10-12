8b7b373706ba07e4a9bbbd829b97378e8b86ea53
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-13 01:50:07 +0530

    Time: 3 ms (59.84%), Space: 54.3 MB (78.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,5 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int cnt0 = 0;
-        for(int it: nums){
-            if(it == 0){
-                cnt0++;
-            }
-        }
         int i = 0;
         for(int it: nums){
             if(it != 0){

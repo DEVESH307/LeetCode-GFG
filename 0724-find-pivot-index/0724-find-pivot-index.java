05e23c48783356b67362289e924f469426c5ac13
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-14 00:30:21 +0530

    Time: 1 ms (100.00%), Space: 52.4 MB (50.24%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,42 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        // int n = nums.length;
+        // int[] leftSum = new  int[n];        
+        // int[] rightSum = new  int[n];
+        // leftSum[0] = nums[0];
+        // rightSum[n-1] = nums[n-1];
+        // for(int i = 1; i < n; i++){
+        //     leftSum[i] = leftSum[i-1] + nums[i];
+        //     rightSum[n-i-1] = rightSum[n-i] + nums[n-i-1];
+        // }
+        // for(int i = 0; i < n; i++){
+        //     if(leftSum[i] == rightSum[i]){
+        //         return i;
+        //     }
+        // }
+        // return -1;
         int n = nums.length;
-        int[] leftSum = new  int[n];        
-        int[] rightSum = new  int[n];
-        leftSum[0] = nums[0];
-        rightSum[n-1] = nums[n-1];
-        for(int i = 1; i < n; i++){
-            leftSum[i] = leftSum[i-1] + nums[i];
-            rightSum[n-i-1] = rightSum[n-i] + nums[n-i-1];
+        int leftSum = 0;
+        int totalSum = 0;
+        for(int it: nums){
+            totalSum += it;
         }
         for(int i = 0; i < n; i++){
-            if(leftSum[i] == rightSum[i]){
+            totalSum -= nums[i];
+            if(leftSum == totalSum){
                 return i;
             }
+            leftSum += nums[i];
         }
         return -1;
     }
 }
+
+
+
+
+
+
+
+
+

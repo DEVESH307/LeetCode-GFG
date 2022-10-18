6806cb6d32304929a6519a9f7ebbbb31787469ee
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-19 03:04:51 +0530

    Time: 138 ms (57.30%), Space: 48.5 MB (24.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
+        if(n == 1)
+            return true;
         int i = 0;
         for (int reach = 0; i < n && i <= reach; ++i){
             reach = max(i + nums[i], reach);

73977157916a01430fd84867578fad5bb5b7d5c1
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-19 00:13:01 +0530

    Time: 137 ms (69.13%), Space: 19.7 MB (95.41%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        int n = nums.size();
+        if(n < 3){
+            return {};
+        }
+        if(nums[0] > 0){
+            return {};
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
+                break;
+            }
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int low = i+1;
+            int high = n-1;
+            int sum = 0;
+            while(low < high){
+                sum = nums[i]+nums[low]+nums[high];
+                if(sum > 0){
+                    high--;
+                }
+                else if(sum < 0){
+                    low++;
+                }
+                else{
+                    res.push_back({nums[i], nums[low], nums[high]});
+                    int last_occr_low = nums[low];                    
+                    int last_occr_high = nums[high];
+                    while(low < high && last_occr_low == nums[low]){
+                        low++;
+                    }
+                    while(low < high && last_occr_high == nums[high]){
+                        high--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};

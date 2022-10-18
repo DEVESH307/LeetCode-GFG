4076ef54ae349d7b2e945255a42b7008a2ab39de
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-19 03:34:33 +0530

    Time: 137 ms (87.95%), Space: 61.2 MB (62.01%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -10,22 +10,38 @@
 // //         return -1;
 // //     }
 // // };
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int slow = nums[0];
+//         int fast = nums[0];
+        
+//         do{
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         }while(slow != fast);
+        
+//         slow = nums[0];
+//         while(slow != fast){
+//             slow = nums[slow];
+//             fast = nums[fast];
+//         }
+//         return slow;
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }while(slow != fast);
-        
-        slow = nums[0];
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        for (int num : nums) {
+            int idx = abs(num);
+            if (nums[idx] < 0) {
+                return idx;
+            }
+            nums[idx] = -nums[idx];
         }
-        return slow;
+
+        return -1;
     }
 };

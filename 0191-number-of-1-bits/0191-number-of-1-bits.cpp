912988d0ba035797552e5603aa11aa9257cf8d31
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-20 01:51:54 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.52%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,12 +1,24 @@
+// class Solution {
+// public:
+//     int hammingWeight(uint32_t n) {
+//         int count = 0;
+//         while(n){
+//             if(n&1){
+//                 count++;
+//             }
+//             n = n>>1;
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n){
-            if(n&1){
-                count++;
-            }
-            n = n>>1;
+            n = (n&(n-1));
+            count++;
         }
         return count;
     }

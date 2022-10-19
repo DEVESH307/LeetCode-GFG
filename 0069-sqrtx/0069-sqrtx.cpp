e46285a0c7b475e2277dd5a6135bc942829a89ca
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-20 00:34:39 +0530

    Time: 4 ms (59.37%), Space: 5.9 MB (25.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,8 +4,7 @@ public:
         int N = INT_MAX;
         int ans = 0;
         int low = 0;
-        int high = x;
-            // min(N, x);
+        int high = min(N, x);
         while(low <= high){
             int mid = (low+high)/2;
             if(mid*1LL*mid <= x){

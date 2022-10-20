9b1110b31a8c610f8182fe402380e6c35acb7531
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-21 03:33:15 +0530

    Time: 152 ms (40.64%), Space: 44.6 MB (22.73%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,12 +1,30 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         vector<int> res;
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         for(int i = 0; i < n-1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 res.push_back(nums[i]);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 0; i < n-1; i++){
-            if(nums[i] == nums[i+1]){
-                res.push_back(nums[i]);
+        unordered_set<int> st;
+        for(auto num: nums){
+            if(st.find(num) != st.end()){
+                res.push_back(num);
+            }
+            else{
+                st.insert(num);
             }
         }
         return res;

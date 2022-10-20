ceda5876bb9fa33a7c009688902d12df6b938f5b
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-21 02:57:35 +0530

    Time: 265 ms (18.23%), Space: 59 MB (80.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int res = INT_MIN;
+//         int left = 0;
+//         int right = height.size()-1;
+//         while(left <= right){
+//             if(height[left] < height[right]){
+//                 res = max(res, height[left]*(right-left));
+//                 left++;
+//             }
+//             else if(height[left] > height[right]){
+//                 res = max(res, height[right]*(right-left));
+//                 right--;
+//             }
+//             else{
+//                 res = max(res, height[left]*(right-left));
+//                 left++;
+//                 right--; 
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -5,18 +30,12 @@ public:
         int left = 0;
         int right = height.size()-1;
         while(left <= right){
+            res = max(res, min(height[left], height[right])*(right-left));
             if(height[left] < height[right]){
-                res = max(res, height[left]*(right-left));
                 left++;
-            }
-            else if(height[left] > height[right]){
-                res = max(res, height[right]*(right-left));
-                right--;
             }
             else{
-                res = max(res, height[left]*(right-left));
-                left++;
-                right--; 
+                right--;
             }
         }
         return res;

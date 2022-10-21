4605794d027882be4f3193cfe754db519db97cf6
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-22 03:32:26 +0530

    Time: 165 ms (40.64%), Space: 13 MB (93.54%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n-3; i++){
+            for(int j = i+1; j < n-2; j++){
+                int left_ptr = j+1;
+                int right_ptr = n-1;
+                while(left_ptr < right_ptr){
+                    // int new_target = target - (nums[i] + nums[j]);
+                    // int curr_sum = nums[left_ptr] + nums[right_ptr]; 
+                    long long curr_sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[left_ptr] + (long long)nums[right_ptr]; 
+                    if(curr_sum > target)
+                        --right_ptr;
+                    else if(curr_sum < target)
+                        ++left_ptr;
+                    else{
+                        res.push_back({nums[i], nums[j], nums[left_ptr], nums[right_ptr]});
+                        ++left_ptr;
+                        --right_ptr;
+                        while (left_ptr < right_ptr && nums[left_ptr] == nums[left_ptr-1]) ++left_ptr;
+                        while (left_ptr < right_ptr && nums[right_ptr] == nums[right_ptr+1]) --right_ptr;
+                    }
+                }
+                while(j + 1 < n && nums[j] == nums[j+1]) ++j;
+            }
+            while(i + 1 < n && nums[i] == nums[i+1]) ++i;
+        }
+        return res;
+    }
+};

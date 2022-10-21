b03eff50342c765b49cdaceb24277801ab62f837
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-21 23:51:06 +0530

    Time: 450 ms (70.76%), Space: 8 MB (79.81%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,33 @@
+// , vector<vector<int>> &dp
+class Solution {
+public:
+    int row[4] = {1, 0, -1, 0};    
+    int col[4] = {0, 1, 0, -1};
+    bool wordSearchExistHelper(vector<vector<char>>& board, string &word, int i, int j, int N, int M, int k){
+        if(k >= word.size())
+            return true;
+        if(i < 0 || i >= N || j < 0 || j >= M || board[i][j] == '.' || word[k] != board[i][j])
+            return false;
+        if(word.size() == 1 && word[k] == board[i][j]) 
+            return true;
+        board[i][j] = '.';
+        int ans = false;
+        for(int index = 0; index < 4; index++)
+            ans |= wordSearchExistHelper(board, word, i+row[index], j+col[index], N, M, k+1);
+        board[i][j] = word[k];
+        return ans;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int N = board.size();    
+        // if(N == 0) return false;
+        int M = board[0].size();
+        int k = word.size();
+        // if(k == 0) return false;
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < M; j++)
+                if(board[i][j] == word[0])
+                    if(wordSearchExistHelper(board, word, i, j, N, M, 0))
+                        return true;
+        return false;
+    }
+};

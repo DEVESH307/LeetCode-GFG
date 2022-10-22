bee2da78e338a59ea33606cd38502059fa48b0d3
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-22 15:25:10 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (70.40%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void subsetsHelper(vector<int>& nums, int index, vector<vector<int>> &res, vector<int> &temp){
+        if(index == nums.size()){
+            res.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[index]);
+        subsetsHelper(nums, index+1, res, temp);
+        temp.pop_back();
+        subsetsHelper(nums, index+1, res, temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        subsetsHelper(nums, 0, res, temp);
+        return res;
+    }
+};

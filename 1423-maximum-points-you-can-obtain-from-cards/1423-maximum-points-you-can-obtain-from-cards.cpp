6e657da3d4474a4856028819a4c3ce318ea3b8a8
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-22 14:29:43 +0530

    Time: 223 ms (5.15%), Space: 42.4 MB (80.22%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,47 +1,18 @@
-// class Solution {
-// public:
-//     int maxScore(vector<int>& cardPoints, int k) {
-//         int n = cardPoints.size();
-//         long long totalSum = 0;
-//         long long minSumOfUnpickedCard;
-//         long long currSumOfUnpickedCard = 0;
-//         for(auto points: cardPoints){
-//             totalSum += points;
-//         }
-//         // cout<<totalSum<<endl;
-//         for(int i = 0; i < n-k; i++){
-//             currSumOfUnpickedCard += cardPoints[i];
-//         }
-//         // cout<<currSumOfUnpickedCard<<endl;
-//         minSumOfUnpickedCard = currSumOfUnpickedCard;
-//         // cout<<minSumOfUnpickedCard<<endl;
-//         for(int i = n-k; i < n; i++){
-//             currSumOfUnpickedCard -= cardPoints[i-(n-k)];
-//             currSumOfUnpickedCard += cardPoints[i];
-//             minSumOfUnpickedCard = min(minSumOfUnpickedCard, currSumOfUnpickedCard);
-//             // cout<<minSumOfUnpickedCard<<endl;
-//         }
-//         return (int) (totalSum - minSumOfUnpickedCard);
-//     }
-// };
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        int res = 0;
-		
-		//First k elements in our window
-        for(int i=0;i<k;i++) res+=cardPoints[i];
-        
-        int curr=res;
-        for(int i=k-1;i>=0;i--) {
-			//We remove the last visited element and add the non-visited element from the last
-            curr-=cardPoints[i];
-            curr+=cardPoints[cardPoints.size()-k+i];
-			
-            //We check the maximum value any possible combination can give
-			res = max(res, curr);
+        int n = cardPoints.size();
+        int currSum = 0;
+        int maxSum;
+        for(int i = 0; i < k; i++){
+            currSum += cardPoints[i];
         }
-        
-        return res;
+        maxSum = currSum;
+        for(int i = k-1; i >= 0; i--){
+            currSum -= cardPoints[i];
+            currSum += cardPoints[n-k+i];
+            maxSum = max(maxSum, currSum);
+        }
+        return maxSum;
     }
 };

e099c265ad264dbe106cd5388abfc99ea3116694
DEVESH KUMAR JAISWAL <dej13121995@gmail.com>
2022-10-22 14:30:15 +0530

    Time: 70 ms (89.52%), Space: 42.4 MB (80.22%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -16,3 +16,30 @@ public:
         return maxSum;
     }
 };
+
+// class Solution {
+// public:
+//     int maxScore(vector<int>& cardPoints, int k) {
+//         int n = cardPoints.size();
+//         long long totalSum = 0;
+//         long long minSumOfUnpickedCard;
+//         long long currSumOfUnpickedCard = 0;
+//         for(auto points: cardPoints){
+//             totalSum += points;
+//         }
+//         // cout<<totalSum<<endl;
+//         for(int i = 0; i < n-k; i++){
+//             currSumOfUnpickedCard += cardPoints[i];
+//         }
+//         // cout<<currSumOfUnpickedCard<<endl;
+//         minSumOfUnpickedCard = currSumOfUnpickedCard;
+//         // cout<<minSumOfUnpickedCard<<endl;
+//         for(int i = n-k; i < n; i++){
+//             currSumOfUnpickedCard -= cardPoints[i-(n-k)];
+//             currSumOfUnpickedCard += cardPoints[i];
+//             minSumOfUnpickedCard = min(minSumOfUnpickedCard, currSumOfUnpickedCard);
+//             // cout<<minSumOfUnpickedCard<<endl;
+//         }
+//         return (int) (totalSum - minSumOfUnpickedCard);
+//     }
+// };
